541f6c43d396b0c603205b8a4082c7026caca2cc
Dennis Jekubczyk <dennis.jekubczyk@gmail.com>
2023-05-07 10:24:18 +0200

    simplify rule

--- src/no-for.js
+++ src/no-for.js
@@ -1,31 +1,25 @@
 exports.create = function (context) {
     return {
         ForStatement: function (node) {
-            if (node.type === "ForStatement") {
-                context.report({
-                    node: node,
-                    message:
-                        "use of for-loops is forbidden",
-                });
-            }
+            context.report({
+                node: node,
+                message:
+                    "use of for-loops is forbidden",
+            });
         },
         ForOfStatement: function (node) {
-            if (node.type === "ForOfStatement") {
-                context.report({
-                    node: node,
-                    message:
-                        "use of for-loops is forbidden",
-                });
-            }
+            context.report({
+                node: node,
+                message:
+                    "use of for-loops is forbidden",
+            });
         },
         ForInStatement: function (node) {
-            if (node.type === "ForInStatement") {
-                context.report({
-                    node: node,
-                    message:
-                        "use of for-loops is forbidden",
-                });
-            }
+            context.report({
+                node: node,
+                message:
+                    "use of for-loops is forbidden",
+            });
         },
     };
 };
--- src/no-while.js
+++ src/no-while.js
@@ -1,13 +1,11 @@
 exports.create = function (context) {
     return {
         WhileStatement: function (node) {
-            if (node.type === "WhileStatement") {
-                context.report({
-                    node: node,
-                    message:
-                        "use of while-loops is forbidden",
-                });
-            }
+            context.report({
+                node: node,
+                message:
+                    "use of while-loops is forbidden",
+            });
         },
     };
 };
