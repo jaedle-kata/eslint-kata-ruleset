de31cb71d0f81442a91c786752c051b0aa4ceb5a
Dennis Jekubczyk <dennis.jekubczyk@gmail.com>
2023-05-08 08:15:29 +0200

    ensure rule is part of the package

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 exports.rules = {
+    'no-array-foreach': require('./src/no-array-foreach'),
     'no-array-push': require('./src/no-array-push'),
     'no-do-while': require('./src/no-do-while'),
     'no-for': require('./src/no-for'),
